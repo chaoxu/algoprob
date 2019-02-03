4df58180dd2f15138bbe3b33defb83903d3a8256
Chao Xu <mgcclx@gmail.com>
2019-02-03 02:24:19 -0500

    add glossary

--- build.py
+++ build.py
@@ -2,8 +2,7 @@ import yaml
 import re
 import mistune
 import functools
-from jinja2 import Template
-
+from jinja2 import Template, Environment, BaseLoader, FileSystemLoader
 mk = mistune.Markdown(parse_block_html=True)
 def compose(*functions):
     return functools.reduce(lambda f, g: lambda x: f(g(x)), functions, lambda x: x)
@@ -53,15 +52,29 @@ def build_problem(problem):
 
     return problem 
 
-def build_problems(problems):
-    return map(build_problem,problems)
+def build_glossary(glossary):
+    #print list(glossary)
+    words = sorted(list(glossary.keys()))
+    z = []
+    for word in words:
+        y = {}
+        y["name"] = mki(math(word))
+        y["definition"]=mki(math(glossary[word]))
+        z.append(y)
+    return z
 
+
+env = Environment(loader=FileSystemLoader(""))
+# build problems
 problems = list(yaml_loader("problems.yaml"))
-# problems = list(yaml_loader("test.yaml")) 
 parsed = map(build_problem,problems)
+template = env.get_template('problems.html')
+with open('_site/index.html', 'w') as file:
+    file.write(template.render(problems=parsed).encode( "utf-8" ))
 
-# print(parsed)
-file = open("template.html", "r") 
-template = Template(file.read())
-
-print(template.render(problems=parsed).encode( "utf-8" ))
+# build glossary
+glossary = list(yaml_loader("glossary.yaml"))[0]
+parsed = build_glossary(glossary)
+template = env.get_template('glossary.html')
+with open('_site/glossary.html', 'w') as file:
+    file.write(template.render(glossary=parsed).encode( "utf-8" ))
--- push.sh
+++ push.sh
@@ -6,19 +6,15 @@ git stash
 git checkout develop
 
 # Build files
-python build.py > _site/index.html
+python build.py
 cp default.css _site/default.css
-cp problems.yaml _site/problems.yaml
-
+cp README.md _site/README.md
 # Get previous files
 git fetch --all
 git checkout -b master --track origin/master
 
 # Overwrite existing files with new files
-mv _site/index.html index.html
-mv _site/default.css default.css
-mv _site/problems.yaml problems.yaml
-
+rsync -a --filter='P _site/' --filter='P .git/' --filter='P .gitignore' --filter='P .gitattributes' --delete-excluded _site/ .
 # Commit
 git add -A
 git commit -m "Publish."
